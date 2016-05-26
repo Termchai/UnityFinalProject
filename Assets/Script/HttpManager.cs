f1a1945451c565b418f5e9cfc2f6a53fb7a20db8
Termchai Sadsaengchan <bik4412@hotmail.com>
2016-05-27 02:07:32 +0700

    Add UnityHttp

---
+++ Assets/Script/HttpManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HttpManager : MonoBehaviour {
+
+    string uri = "https://infinite-gorge-23096.herokuapp.com/";
+	void Start()
+    {
+        Debug.Log("Test Http Manager");
+        //scanUser();
+        writeUser();
+    }
+
+    public void scanUser()
+    {
+        HTTP.Request someRequest = new HTTP.Request("get", uri + "users");
+        someRequest.Send((request) => {
+            JSONObject thing = new JSONObject(request.response.Text);
+            for(int i=0; i<thing.list.Count; i++)
+            {
+                Debug.Log(thing.list[i]["AndroidId"]);
+            }
+        });
+    }
+
+    public void writeUser()
+    {
+        Hashtable data = new Hashtable();
+        data.Add("AndroidId", "test");
+        data.Add("GCMToken", "test2");
+
+        HTTP.Request theRequest = new HTTP.Request("post", uri + "users", data);
+        theRequest.Send((request) => {
+
+
+            Hashtable result = request.response.Object;
+            if (result == null)
+            {
+                Debug.LogWarning("Could not parse JSON response!");
+                
+                return;
+            }
+            Debug.Log(request.response.Text);
+
+        });
+    }
+
+
+}
